48312d2f177474577619b58bcd5b3fd95e576a94
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2022-09-19 16:04:40 -0500

    2 reducer en un State

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,9 @@ import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
-import { todoReducer } from './todos/todo.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { appReducers } from './app.reducer';
 
 @NgModule({
   declarations: [
@@ -22,7 +22,7 @@ import { environment } from '../environments/environment';
     BrowserModule,
     ReactiveFormsModule,
     TodoModule,
-    StoreModule.forRoot({todos: todoReducer}, {}),
+    StoreModule.forRoot(appReducers),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
