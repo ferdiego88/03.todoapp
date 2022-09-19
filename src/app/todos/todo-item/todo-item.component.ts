992943274dc70d7b9abdf6c37ae262605a43b154
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2022-09-19 12:59:49 -0500

    controlar Todo Items

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Todo } from '../models/todo.model';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() todo!: Todo;
+  @ViewChild('inputFisico') txtInputFisico!: ElementRef;
+
+  chkCompletado!: FormControl;
+  txtInput!: FormControl;
+
+  editando = false;
+
+  constructor() {
+
+   }
+
 
   ngOnInit(): void {
+
+    this.chkCompletado = new FormControl(this.todo.completado);
+    this.txtInput = new FormControl(this.todo.texto, Validators.required);
+
+  }
+
+
+  editar() {
+    this.editando = true;
+
+    setTimeout(() => {
+      this.txtInputFisico.nativeElement.select();
+    }, 200);
+
+  }
+
+
+  terminarEdicion() {
+    this.editando = false;
   }
 
 }
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -3,7 +3,10 @@ import { crear } from './todo.actions';
 import { Todo } from './models/todo.model';
 
 export const initialState: Todo[] = [
-  new Todo('Salvar al Mundo')
+  new Todo('Salvar al Mundo'),
+  new Todo('Vencer a Thanos'),
+  new Todo('Fabricar traje de IronMan'),
+  new Todo('Robar escudo de Capitán America'),
 ];
 
 export const todoReducer = createReducer(
